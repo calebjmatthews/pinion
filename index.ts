12b3c8ee08b494fe41e6097e9039ec761d01c095
Caleb Matthews <calebjmatthews@gmail.com>
2025-05-07 16:09:32 -0500

    feat: templating with eta

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import Bun from 'bun';
+import { Eta } from "eta";
+import path from "path";
+
+const eta = new Eta({ views: path.join(__dirname, "templates") });
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    // Static routes
+    "/api/status": new Response("OK"),
+    "/*": () => new Response(
+      eta.render("./simple", { names: ["Carb", "Erm", "Haruki", "Mazel"] }), 
+      { headers: { "Content-Type": "text/html" } }
+    )
+  }
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
