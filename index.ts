5eb73b5c14a060f41126c20e6bd5e6bbd802d58f
Caleb Matthews <calebjmatthews@gmail.com>
2025-05-09 13:42:34 -0500

    feat: css bundling and css reset

--- index.ts
+++ index.ts
@@ -4,12 +4,23 @@ import path from "path";
 
 import postsGet from "./connectors/postsGet";
 
+await Bun.build({
+  entrypoints: ['./styles.ts'],
+  outdir: './out',
+});
+
 const eta = new Eta({ views: path.join(__dirname, "views") });
 
 const server = Bun.serve({
   port: 3000,
   routes: {
     "/api/status": new Response("OK"),
+
+    "/styles.css": new Response(
+      await Bun.file("./out/styles.css").bytes(),
+      { headers: { "Content-Type": "text/css" } }
+    ),
+
     "/*": () => {
       const posts = postsGet();
       return new Response(
