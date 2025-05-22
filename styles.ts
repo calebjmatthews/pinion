cd4b9e19bf2fac35aaa216d6a3f963ce61bd0913
Caleb Matthews <calebjmatthews@gmail.com>
2025-05-22 15:05:17 -0500

    work: header styling

--- styles.ts
+++ styles.ts
@@ -4,3 +4,4 @@ import "./views/components/post/post.css";
 import "./views/components/post_list/post_list.css";
 import "./views/components/post_new/post_new.css";
 import "./views/components/log_in/log_in.css";
+import "./views/components/header/header.css";
